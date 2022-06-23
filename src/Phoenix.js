a2ac15df5338a3934cfffb4707586d3aee0be7a8
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2022-06-23 17:13:22 +0200

    fix permissions

--- src/Phoenix.js
+++ src/Phoenix.js
@@ -55,15 +55,50 @@ module.exports = class Phoenix {
 
         Object.keys(Commands).forEach(element => {
             if (Commands[element].match(command)) {
-                // if (!searchPermissions(Commands[element], message)) {
-                //     PermissionDenied(message);
-                //     return;
-                // }
+                if (!this.searchPermissions(Commands[element], message, phoenixGuild)) {
+                    console.log('Permission denied');
+                    message.reply("Patouche");
+                    return;
+                }
                 if (!message.guild && (typeof Commands[element].callableFromMP == 'undefined' || !Commands[element].callableFromMP))
                     return
                 Commands[element].call(message, this);
             }
         });
+    }
+
+    searchPermissions(command, message, phoenixGuild) {
+        for (let name of Object.keys(phoenixGuild.config.permissions)) {
+            if (name === command.name) {
+                let perm = phoenixGuild.config.permissions[name];
+                return this.checkPermissions(perm, message);
+            }
+        }
+        return this.checkPermissions(phoenixGuild.config.permissions.default, message);
+    }
+
+    checkPermissions(perm, message) {
+        let member = message.member;
+        let role = member.roles.highest;
+        // check blacklists
+        if (perm.roles.blacklist.length > 0 && perm.roles.blacklist.includes(role.name)) {
+            return false;
+        }
+        if (perm.channels.blacklist.length > 0 && perm.channels.blacklist.includes(message.channel.id)) {
+            return false;
+        }
+        if (perm.members.blacklist.includes(message.author.tag)) {
+            return false;
+        }
+
+        // check whitelists
+        if (perm.roles.whitelist.length > 0 && !perm.roles.whitelist.includes(role.name)) {
+            return false;
+        }
+        if (perm.channels.whitelist.length > 0 && !perm.channels.whitelist.includes(message.channel.id)) {
+            return false;
+        }
+        return !(perm.members.whitelist.length > 0 && !perm.members.whitelist.includes(message.author.tag));
     }
 
     sendClean(msg, channel, time = 20000) {
