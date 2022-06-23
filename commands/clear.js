0d71d56f24e1da1893af15a931693c7c21d34b5b
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2022-06-23 17:18:54 +0200

    fix clear

--- commands/clear.js
+++ commands/clear.js
@@ -9,9 +9,10 @@ module.exports = class Clear extends Command {
     static description = "Nettoie le chat des commandes bot";
 
     static async call(message, phoenix) {
-        let messages = message.channel.messages.filter(msg => msg.author.id === phoenix.bot.user.id || msg.content.startsWith(phoenix.config.prefix));
-        for(let msg of messages) {
-            await this.sleep(1000);
+        let allMessage = await message.channel.messages.fetch();
+        const prefix = phoenix.guilds[message.guildId].config.prefix;
+        let botMessages = allMessage.filter(msg => msg.author.id === phoenix.bot.user.id || msg.content.startsWith(prefix));
+        for (let msg of botMessages) {
             msg[1].delete().catch(err => {
                 console.error(err);
             })
