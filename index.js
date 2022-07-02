72f6ac3e0ddc9d86c4f74f3f90188d63bce961ab
Tony Heng <tony.heng@epita.fr>
2022-07-02 21:28:58 +0200

    feat: sentry: add phoenix transaction

--- index.js
+++ index.js
@@ -2,14 +2,33 @@
 require("./src/http");
 const Phoenix = require("./src/Phoenix");
 
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
+Sentry.init({
+    dsn: "https://c6553244713742d693519319a55718ba@o1287280.ingest.sentry.io/6545421",
+    tracesSampleRate: 1.0,
+});
+
+const transaction = Sentry.startTransaction({
+    op: "phoenix",
+    name: "Phoenix Bot Transaction",
+});
+
 RegExp.escape = function (s) {
     return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
 };
 
-const phoenix = new Phoenix();
+try {
+    const phoenix = new Phoenix();
 
-phoenix.loadConfig().then(async () => {
-    await phoenix.login();
-});
+    phoenix.loadConfig().then(async () => {
+        await phoenix.login();
+    });
+} catch (e) {
+    Sentry.captureException(e);
+} finally {
+    transaction.finish();
+}
+
 
 module.exports = phoenix;
