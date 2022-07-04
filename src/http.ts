15844af3d3bf6754393791d85d5458fb0af3aab9
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2022-07-04 11:37:41 +0200

    http: fix absolute path

--- src/http.ts
+++ src/http.ts
@@ -8,11 +8,21 @@ const app = express();
 import config from "../config.json" assert { type: "json" };
 
 router.get("/mp3/:file", (req, res) => {
-    res.sendFile(path.join(__dirname, `../public/${req.params.file}.mp3`));
+    const filename: string = req.params.file;
+    if (!filename.match(/\d{6}/)) {
+        res.send('Invalid video');
+        return;
+    }
+    res.sendFile(path.resolve(`./public/${req.params.file}.mp3`));
 });
 
 router.get("/mp4/:file", (req, res) => {
-    res.sendFile(path.join(__dirname, `../public/${req.params.file}.mp4`));
+    const filename: string = req.params.file;
+    if (!filename.match(/\d{6}/)) {
+        res.send('Invalid video');
+        return;
+    }
+    res.sendFile(path.resolve(`./public/${req.params.file}.mp4`));
 });
 
 logger.info("Starting http server", { label: "HTTP" });
